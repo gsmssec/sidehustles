a3746d2f9ee8ea15945b5d4c5e26d0d80f9e71e7
Giridhar Murthy Sreedhara <gsmssec@gmail.com>
2025-06-21 19:31:22 -0400

    All

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
-user_text = input("Enter a Todo: ")
-str_len = len(user_text)
-print(f"User Entered {user_text}")
-print(f"The length of the string entered is {str_len}")
+import os
+import sys
+import time
+import random
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import requests
+import json
+import re   
+import datetime
+import pytz
+import logging
+import sqlite3
+import hashlib
+
+
+
+print("Hello " + input("Enter your name: ") + "!")
+var1  = input("Enter a string:")
+
+print("The length of the string is :" + str(len(var1)))
+
+if len(var1) > 5:
+    print("The string is longer than 5 characters.")
+else:
+    print("The string is 5 characters or shorter.")
+    
